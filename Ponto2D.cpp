9caa613ef88001ce9260984ff205e71c06da1623
ruybragafilho <ruybragafilho@gmail.com>
2022-05-22 00:57:38 -0300

    Atualizacao Ponto2D para usar grid

--- Ponto2D.cpp
+++ Ponto2D.cpp
@@ -1,23 +1,21 @@
-/*  Definicoes dos métodos do tipo abstrato de dados Ponto2D,        */
-/*  utilizado para representar um ponto (x, y) no plano cartesiano.  */
-/*                                                                   */
-/*  Autor: Ruy Braga Filho                                           */
+/*  Definicoes dos métodos do tipo abstrato de dados Ponto2D,     */
+/*  utilizado para representar um ponto (linha, coluna) no grid.  */
+/*                                                                */
+/*  Autor: Ruy Braga Filho                                        */
 
 
 
 #include <sstream>
-#include <iomanip>
-#include <cmath>
 #include "Ponto2D.h"
 
 
 
-/* métodos Sets e Gets para as coordenadas (x, y) */
-void Ponto2D::setX( long x ) { this->x = x; }
-long Ponto2D::getX() { return x; }
+/* métodos Sets e Gets para as coordenadas (linha, coluna) */
+void Ponto2D::setLinha( long linha ) { this->linha = linha; }
+long Ponto2D::getLinha() { return linha; }
 
-void Ponto2D::setY( long y ) { this->y = y; }
-long Ponto2D::getY() { return y; }
+void Ponto2D::setColuna( long coluna ) { this->coluna = coluna; }
+long Ponto2D::getColuna() { return coluna; }
 
 
 
@@ -27,50 +25,11 @@ std::string Ponto2D::toString()  {
 
     std::ostringstream aux;
     
-    aux << "(" << this->x << "," << this->y << ")";
+    aux << "(" << this->linha << "," << this->coluna << ")";
 
     return( aux.str() );
 
 }  /*  Fim da definicao do metodo toString.  */
-
-
-
-/* A distância entre pontos de duas dimensões é calculada da seguinte forma:  */
-/* d = sqrt((x1 - x2)^2 + (y1 - y2)^2).                                       */
-double Ponto2D::distanciaEuclidiana( Ponto2D p )  { 
-    
-    return sqrt( static_cast<double>(  (this->x - p.x ) * (this->x - p.x)  + 
-                                       (this->y - p.y ) * (this->y - p.y)  )); 
-                                       
-}  /*  Fim da definicao do metodo distanciaEuclidiana.  */
-
-
-
-/* A distância entre pontos em um grid de duas dimensões é calculada da seguinte forma: */
-/* d = menor(deltaX, deltaY) + ( maior(deltaX, deltaY) - menor(deltaX, deltaY) )        */
-/* onde deltaX = abs( x2 - x1 - 1) e deltaY = abs( y2 - y1 - 1)                         */
-long Ponto2D::distanciaGrid( Ponto2D p )  {
-
-    long deltaX = abs( this->x - p.x ) - 1;
-    long deltaY = abs( this->y - p.y ) - 1;
-
-    long maior;
-    long menor;
-
-    if( deltaX > deltaY )  {
-
-        maior = deltaX;
-        menor = deltaY;
-
-    } else {
-
-        maior = deltaY;
-        menor = deltaX;
-    }
-
-    return ( menor + (maior - menor) );
-
-}  /*  Fim da definicao do metodo distanciaGrid.  */
 
 
 /*  Fim das definicoes dos métodos do tipo abstrato de dados Ponto2D  */
--- Ponto2D.h
+++ Ponto2D.h
@@ -1,7 +1,7 @@
-/*  Cabecalho com a declaracao do tipo abstrato de dados Ponto2D,    */
-/*  utilizado para representar um ponto (x, y) no plano cartesiano.  */
-/*                                                                   */
-/*  Autor: Ruy Braga Filho                                           */
+/*  Cabecalho com a declaracao do tipo abstrato de dados Ponto2D,   */
+/*  utilizado para representar um ponto (linha, coluna) no grid.    */
+/*                                                                  */
+/*  Autor: Ruy Braga Filho                                          */
 
 
 
@@ -13,46 +13,34 @@
 
 
 /**
- * \Um ponto bidimensional.
+ * Um ponto bidimensional.
  */
 class Ponto2D {
 	
     protected:
 
-        /* Coordenadas (x, y) do ponto */
-	    long x;
-        long y;	
+        /* Coordenadas (linha, coluna) do ponto no grid */
+	    long linha;
+        long coluna;	
 
     public:
 
         /* Construtores da classe Ponto2D */
-		Ponto2D(): x(0), y(0) {}
-        Ponto2D(long x, long y): x(x), y(y) {}
+		Ponto2D(): linha(0), coluna(0) {}
+        Ponto2D(long linha, long coluna): linha(linha), coluna(coluna) {}
 
         
-        /* métodos Sets e Gets para as coordenadas (x, y) */
-        void setX( long x );
-        long getX();
+        /* métodos Sets e Gets para as coordenadas (linha, coluna) */
+        void setLinha( long linha );
+        long getLinha();
 
-		void setY( long y );
-		long getY();
+		void setColuna( long coluna );
+		long getColuna();
 		
-
 	    
         /* Representação de string do ponto.                                          */
         /* retorna uma string que representa o ponto, como uma tupla de n-dimensões.  */      
         std::string toString();
-    
-        
-    	/* A distância entre pontos de duas dimensões é calculada da seguinte forma:  */
-        /* d = sqrt((x1 - x2)^2 + (y1 - y2)^2).                                       */
-        double distanciaEuclidiana( Ponto2D p );
-
-
-    	/* A distância entre pontos em um grid de duas dimensões é calculada da seguinte forma: */
-        /* d = menor(deltaX, deltaY) + ( maior(deltaX, deltaY) - menor(deltaX, deltaY) )        */
-        /* onde deltaX = abs( x2 - x1 - 1) e deltaY = abs( y2 - y1 - 1)                         */
-        long distanciaGrid( Ponto2D p );        
 };
 
 #endif
